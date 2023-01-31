13884074e75554b25070aa7c49ab380336c1e01c
Agnes Kovacs <agnes.simonnekovacs@googlemail.com>
2023-01-31 11:10:56 +0400

    Initial commit

---
+++ simple_vnet.tf
@@ -0,0 +1,65 @@
+provider "azurerm" {
+  version = "2.0"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefix       = "10.0.1.0/24"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_route_table" "example" {
+  name                = "example-route-table"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  route {
+    name                   = "example-route"
+    address_prefix        = "0.0.0.0/0"
+    next_hop_type         = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.0.1"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "example" {
+  subnet_id             = azurerm_subnet.example.id
+  route_table_id        = azurerm_route_table.example.id
+}
+
+resource "azurerm_lb" "example" {
+  name                = "example-lb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "example-fe-ip"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
